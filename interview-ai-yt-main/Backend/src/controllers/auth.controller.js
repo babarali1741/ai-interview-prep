23d5d474d5c8bbe8e1f87fbbbedfe043eb27d726
Babar ali <babarali923889@gmail.com>
2026-07-03 01:50:01 +0530

    fix cookie samesite to none

--- interview-ai-yt-main/Backend/src/controllers/auth.controller.js
+++ interview-ai-yt-main/Backend/src/controllers/auth.controller.js
@@ -44,8 +44,8 @@ async function registerUserController(req, res) {
 
     res.cookie("token", token, {
         httpOnly: true, // JS se cookie access nahi hogi, XSS se safe
-        sameSite: "lax",
-        secure: process.env.NODE_ENV === "production" //  pe http chalega, production mein https zaroori
+        sameSite: "none",
+        secure: true, //  pe http chalega, production mein https zaroori
     })
 
 
